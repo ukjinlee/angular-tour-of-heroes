3b5ec8feb5c05fb1aec894e2b677a0e5e2c370c9
ukjinlee <asllan77@gmail.com>
2018-12-03 15:45:48 -0500

    Add getHeroes()

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,12 +9,16 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
+  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {}
+  
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
-  selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
